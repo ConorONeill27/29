939ed3008fc9c0e2505167dda808b561bce8cceb
Luke Bailey <baileylu@tcd.ie>
2025-02-22 20:55:08 +0000

    Setup header properly so the rest of the site renders

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   # Custom authentication methods or before_actions (if needed)
-  
+
   private
-  
+
   def authenticate_user!
     redirect_to new_session_path, alert: "Please log in." unless user_signed_in?
   end
@@ -13,5 +13,8 @@ class ApplicationController < ActionController::Base
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def not_found
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,19 +2,19 @@ Rails.application.routes.draw do
   resource :session
   resources :passwords, param: :token
 
-  get "up" => "rails/health#show", as: :rails_health_check
+  root "home#index"
 
-  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  get "up" => "rails/health#show", :as => :rails_health_check
+
+  get "service-worker" => "rails/pwa#service_worker", :as => :pwa_service_worker
+  get "manifest" => "rails/pwa#manifest", :as => :pwa_manifest
 
   Rails.application.routes.draw { resources :organizations }
   Rails.application.routes.draw { resources :notes }
   Rails.application.routes.draw { resources :profiles }
 
-  # Define the homepage route
-  get "/", to: "home#index"
-
   # Add a route for "My Hub"
   get "/my_hub", to: "my_hubs#index", as: :my_hub
 
+  match "*path", to: "application#not_found", via: :all
 end
