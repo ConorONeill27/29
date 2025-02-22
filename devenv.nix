f917d4799f1de07e10563c3d771c6b535a2f7c06
Luke Bailey <baileylu@tcd.ie>
2025-02-22 14:40:57 +0000

    setup rails devenv environment

--- devenv.nix
+++ devenv.nix
@@ -1,10 +1,34 @@
-{pkgs, ...}: {
-  languages = {
-    javascript = {
-      enable = true;
-      bun.enable = true;
-      npm.enable = true;
-    };
-    typescript.enable = true;
+{ pkgs, ... }: {
+  packages = with pkgs; [
+    git
+    ruby_3_3
+    pkg-config
+    libyaml.dev
+    openssl.dev
+  ];
+
+  enterShell = ''
+    git --version
+    ruby --version
+    bundle
+  '';
+
+  languages.ruby = {
+    enable = true;
+    package = pkgs.ruby_3_3;
   };
+
+  services.postgres = {
+    enable = true;
+    initialDatabases = [
+      {
+        name = "vocanote";
+        user = "voca";
+        pass = "note";
+      }
+    ];
+    listen_addresses = "127.0.0.1";
+  };
+
+  processes.rails.exec = "rails server";
 }
