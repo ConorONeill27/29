bdf4fd7a93bc3df9fa5735f0be9b5c52a73f74ff
Your Name <your.email@example.com>
2025-02-23 07:48:34 +0000

    Avatars and Maps

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,8 @@ Rails.application.routes.draw do
   get "/about_us", to: "about_us#index", as: :about_us
   get "/my_hub", to: "my_hubs#index", as: :my_hub
     post "/my_hub/chat_message", to: "my_hubs#chat_message"
-  
+  get '/my_hub/graph_data', to: 'my_hubs#graph_data', as: :my_hub_graph_data
+
   resources :organizations
   resources :notes
   resources :profiles
