3df64e4bd52b7a3feeef83a10f67dcc62195a6a2
Your Name <your.email@example.com>
2025-02-23 02:08:56 +0000

    Fixed so much shit

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,13 @@ Rails.application.routes.draw do
 
   root "about_us#index"
 
-  get "up" => "rails/health#show", :as => :rails_health_check
+  get "up" => "rails/health#show", as: :rails_health_check
+  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
-  get "service-worker" => "rails/pwa#service_worker", :as => :pwa_service_worker
-  get "manifest" => "rails/pwa#manifest", :as => :pwa_manifest
+  resources :organizations
+  resources :notes
+  resources :profiles
 
   Rails.application.routes.draw { resources :organizations }
   Rails.application.routes.draw { resources :notes }
@@ -15,7 +18,10 @@ Rails.application.routes.draw do
   Rails.application.routes.draw { resources :recordings }
 
   # Add a route for "My Hub"
+  # Specific routes should come before catch-all routes:
   get "/my_hub", to: "my_hubs#index", as: :my_hub
-  get "/about_us", to: "about_us#index", as: :about_us
+  get "/about", to: "about_us#index", as: :about_us
+
+  # Catch-all route (should be the very last route)
   match "*path", to: "application#not_found", via: :all
 end
